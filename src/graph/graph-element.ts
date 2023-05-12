3564c196f2579f7a730de6f502b4a7e59fcb3389
dazraf <git@dazraf.io>
2023-05-12 18:15:39 +0100

    logic for backward edges

--- src/graph/graph-element.ts
+++ src/graph/graph-element.ts
@@ -147,7 +147,10 @@ class GraphElement extends LitElement {
         })
 
         this.graph.edges.forEach(edge => {
-            if (edge.fromPort.portType == PortType.Output && edge.toPort.portType == PortType.Input) {
+            if (edge.fromPort.portType != PortType.Output || edge.toPort.portType != PortType.Input) {
+                throw new Error(`Invalid edge: ${edge}`)
+            }
+            if (edge.fromPort.node.layer < edge.toPort.node.layer) {
                 const startPort = edge.fromPort
                 const endPort = edge.toPort
                 const sx = startPort.x + startPort.width / 2
@@ -155,9 +158,19 @@ class GraphElement extends LitElement {
                 const ex = endPort.x + startPort.width / 2
                 const ey = endPort.y
                 const my = (sy + ey) / 2
-                const pathDefinition = `M ${sx} ${sy} L ${sx} ${my} L ${ex} ${my} L ${ex} ${ey}`
-                edge.pathDefinition = pathDefinition
+                edge.pathDefinition = `M ${sx} ${sy} L ${sx} ${my} L ${ex} ${my} L ${ex} ${ey}`
+            } else {
+                const sp = edge.fromPort
+                const ep = edge.toPort
+                const sx = sp.x + sp.width / 2
+                const sy = sp.y + sp.height
+                const ex = ep.x + sp.width / 2
+                const ey = ep.y
+                const leftDelta = Math.max(sp.node.layer, ep.node.layer) * 50
+
+                edge.pathDefinition = `M ${sx} ${sy} L ${sx} ${sy + sp.height} L ${sx - leftDelta} ${sy + sp.height} L ${sx - leftDelta} ${ey - ep.height} L ${ex} ${ey - ep.height} L ${ex} ${ey}`
             }
+
         });
     }
 
